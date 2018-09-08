023a5b3371ccc35f2d28d7acfd223d01e5c0d9e3
Miguel Luiz <milap@outlook.com.br>
2018-09-08 10:48:20 -0400

    add images and its configuration for jobs

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Job
+## job
+
+admin.site.register(Job)
+
+
--- jobs/models.py
+++ jobs/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 class Job(models.Model):
   image = models.ImageField(upload_to='images/')
-  summary = models.CharField(max_length=200)
+  summary = models.CharField (max_length=200)
 
 
